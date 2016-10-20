fa0d054ee4285841b383b895d7c3ad7ec51a5fb9
aleksandr106 <igor.glazov1907@gmail.com>
2016-10-20 16:20:43 +0400

    Update vector3d.hpp

--- vector3d.hpp
+++ vector3d.hpp
@@ -9,7 +9,16 @@ public:
     const static vector3d Y;
     const static vector3d Z;
     const static vector3d ZERO;
-
+    int x;
+    int y;
+    int z;
+    vector3d(int x, int y, int z);
+    vector3d operator + (vector3d a) {
+        return vector3d(x+a.x,y+a.y,z+a.z);
+    }
+    vector3d operator - (vector3d a) {
+        return vector3d(x-a.x,y-a.y,z-a.z);
+    }
     // TODO: дописать
 
 };
